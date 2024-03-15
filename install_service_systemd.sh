d241ff190270b3ddb1e02bb58c3cced0fd98d80c
Fabio Toniolo Vieira Junior <ftoniolo@viemar.com.br>
2024-03-15 07:01:16 -0300

    alterado instalador systemd

--- install_service_systemd.sh
+++ install_service_systemd.sh
@@ -1,6 +1,11 @@
 #scp -r -P 55323 root@silverado:/mnt/dados/dados/Ti/python/jupyter/halter /opt/halter
 
+python3 -m venv .venv_linux
+source .venv_linux/bin/activate
+pip3 install -r requirements.txt
+chmod +x halter.py
+
 cp /opt/halter/halter.service /etc/systemd/system/
-chmod +x halter.sh
+#chmod +x halter.sh
 systemctl enable halter
 systemctl start halter
