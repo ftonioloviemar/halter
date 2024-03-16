6f33c026cb74241e26bdb735312acb976aa5e002
Fabio Toniolo Vieira Junior <ftoniolo@viemar.com.br>
2024-03-15 21:28:40 -0300

    ajuste no instalador pfsense

--- install_pfsense.sh
+++ install_pfsense.sh
@@ -4,9 +4,8 @@
 
 #pkg install git
 #git clone https://ftonioloviemar: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@github.com/ftonioloviemar/halter.git
-
-
 #cd halter
+
 python3 -m venv .venv_linux
 source .venv_linux/bin/activate.csh
 pip3 install -r requirements.txt
