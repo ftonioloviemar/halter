d73065efa45bd7a65ce32ec91bc30ce7d03c43d8
Fabio Toniolo Vieira Junior <ftoniolo@viemar.com.br>
2024-03-16 15:53:14 -0300

    removido secret porque agora é público

--- install_pfsense.sh
+++ install_pfsense.sh
@@ -1,12 +1,3 @@
-#service halter stop
-#cd /opt
-#mv halter halter_old
-
-#pkg install -y git
-#git clone https://ftonioloviemar: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@github.com/ftonioloviemar/halter.git
-#cd halter
-#source install_pfsense.sh
-
 python3 -m venv .venv_linux
 source .venv_linux/bin/activate.csh
 pip3 install -r requirements.txt
