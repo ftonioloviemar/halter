01cc5d90e84fb7c6fe7d392052a9f6d8f96c7873
Fabio Toniolo Vieira Junior <ftoniolo@viemar.com.br>
2024-03-17 07:27:22 -0300

    ajuste para voltar ao halter.sh

--- pre_install.sh
+++ pre_install.sh
@@ -1,3 +1,6 @@
+# usar o comando abaixo que dispara este script para instalar / atualizar
+# source <(curl -s https://raw.githubusercontent.com/ftonioloviemar/halter/main/pre_install.sh)
+
 cd /opt
 rm -fdr halter
 yum -y install git
